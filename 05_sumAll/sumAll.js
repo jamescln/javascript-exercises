5a5bbe0f49d14fc2ab84cfd4b4be62f877626f56
James <james.jcarline@gmail.com>
2022-11-08 17:30:45 +0000

    Create function to pass tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  let result;
+  if (
+    num1 >= 0 &&
+    num2 >= 0 &&
+    typeof num1 === "number" &&
+    typeof num2 === "number"
+  ) {
+    if (num1 < num2) {
+      result = num1;
+      for (let i = num1 + 1; i <= num2; i++) {
+        result += i;
+      }
+    } else if (num1 > num2) {
+      result = num2;
+      for (let i = num2 + 1; i <= num1; i++) {
+        result += i;
+      }
+    }
+  } else {
+    return "ERROR";
+  }
 
+  return result;
 };
 
 // Do not edit below this line
