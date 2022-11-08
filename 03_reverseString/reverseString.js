f3411785a816add6d2dee4f8191daccf729a7752
James <james.jcarline@gmail.com>
2022-11-08 16:46:04 +0000

    Create funtion to pass tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  let stringArr = string.split("");
+  let returnString = "";
+  for (let i = stringArr.length - 1; i >= 0; i--) {
+    returnString += stringArr[i];
+  }
+  return returnString;
 };
 
 // Do not edit below this line
