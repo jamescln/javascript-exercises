d4d09575b39fa36bcb28245be74ecc149da9dead
James <james.jcarline@gmail.com>
2022-11-08 16:15:33 +0000

    Created function that passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  if (num >= 0) {
+    let returnString = "";
+    for (let i = 0; i < num; i++) {
+      returnString += string;
+    }
+    return returnString;
+  } else {
+    return "ERROR";
+  }
 };
 
 // Do not edit below this line
