d23d0503c6209557443fa58e9f174d1fb6488682
James <james.jcarline@gmail.com>
2022-11-08 17:06:22 +0000

    Create function to pass test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (inputArray, ...removalTargets) {
+  for (let i = 0; i < removalTargets.length; i++) {
+    for (let j = 0; j < inputArray.length; j++) {
+      if (inputArray[j] === removalTargets[i]) {
+        inputArray.splice([j], 1);
+      }
+    }
+  }
+  return inputArray;
 };
 
 // Do not edit below this line
