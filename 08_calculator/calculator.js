b2ffae701501f9ce435c0f8a90d86f568e5ecaa1
James <james.jcarline@gmail.com>
2022-11-17 18:33:49 +0000

    Complete add, subtract, sum and multiply

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,31 @@
-const add = function() {
-	
+const add = function (...args) {
+  const result = args.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue;
+  }, 0);
+  return result;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (number) {
+  const result = number.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue;
+  }, 0);
+  return Number(result);
 };
 
-const multiply = function() {
-
+const multiply = function (args) {
+  const result = args.reduce((accumulator, currentValue) => {
+    return accumulator * currentValue;
+  }, 1);
+  return result;
 };
 
-const power = function() {
-	
-};
+const power = function () {};
 
-const factorial = function() {
-	
-};
+const factorial = function () {};
 
 // Do not edit below this line
 module.exports = {
@@ -29,5 +34,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
